309983974eb06ab829d8f88fd67549f4f664b6bb
kausalliya <kausalliya16@gmail.com>
2023-03-09 22:09:39 +0530

    commit 2

--- flightapp/urls.py
+++ flightapp/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('Admin-DashBoard',views.dashboard,name='dashboard'),
+    path('seatbooking',views.seatbooking,name='seatbooking'),
 ]
--- flightapp/views.py
+++ flightapp/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 def dashboard(request):
     
     return render(request,'admin/admindashboard.html')
+def seatbooking(request):
+    
+    return render(request,'web/seatbooking.html')
+
    
